bb88de897c1c11cef8684064e0e155b4f0238e92
Aalin41 <s29223647@gmail.com>
2025-11-21 00:58:12 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 確保相對路徑正確，利於部署
+  base: './', // 使用相對路徑，增加 PWA 與部署的相容性
   build: {
     outDir: 'dist',
   }
